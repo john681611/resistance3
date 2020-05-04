8f46a64619778eaaae1031b7ac00d5b877a4cedd
john681611 <john681611@hotmail.com>
2020-05-04 08:13:02 +0100

    version prep to 3.5.2

--- build.py
+++ build.py
@@ -1,12 +1,12 @@
 from shutil import rmtree
 from distutils.dir_util import copy_tree
 from os import listdir, getcwd
-rmtree(getcwd()+ '\\built')
+rmtree(getcwd()+ '/built')
 print('removing built')
 for f in listdir('./maps'):
-    map = (getcwd()+'\\maps\\'+f)
-    built = (getcwd()+'\\built\\co_36_resistance3-5-1'+f)
-    commonFile = (getcwd()+'\\common')
+    map = (getcwd()+'/maps/'+f)
+    built = (getcwd()+'/built/co_36_resistance3-5-2'+f)
+    commonFile = (getcwd()+'/common')
     copy_tree(commonFile, built)
     copy_tree(map, built)
     print('built:'+ built)
